b7310e8e9464af0cd602acb3ed0d2b04f231c2be
Barlas Söker <bsoker98@gmail.com>
2026-07-15 19:03:33 +0200

    cleanup FS code

--- tests/test-progs/ndp/ndp_dev_a/main.cpp
+++ tests/test-progs/ndp/ndp_dev_a/main.cpp
@@ -35,28 +35,8 @@
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL); // Flush stdout immediately
 
-#ifdef FS
-  int fd_dev_mem = open("/dev/mem", O_RDWR | O_SYNC);
-  assert(fd_dev_mem);
-
-  uint64_t *ndp_mreg =
-      (uint64_t *)mmap(NULL, (size_t)NDP_TSZE, PROT_READ | PROT_WRITE,
-                       MAP_SHARED, fd_dev_mem, NDP_CTRL);
-
-#ifdef DRIVER
-  uint64_t ndp_ctrl[7];
-
-  int fd_ndp_dev_a = open(FID_NAME, O_RDWR | O_SYNC);
-  assert(fd_ndp_dev_a);
-#else
-  uint64_t *ndp_ctrl = ndp_mreg;
-#endif
-
-  uint64_t *ndp_data = ndp_mreg + (NDP_CSZE / sizeof(uint64_t));
-#else
   uint64_t *ndp_ctrl = (uint64_t *)NDP_CTRL;
   uint64_t *ndp_data = (uint64_t *)NDP_DATA;
-#endif
 
   // Initialize data for legacy queries
   for (int i = 0; i < DATA_SIZE; ++i) {
@@ -91,128 +71,92 @@ int main(int argc, char *argv[]) {
 
   /* ============================== Hit_TSC_Best ==============================
    */
-  //   auto start_sw = GET_TICKS;
-  //   uint64_t res_sw = compare_n_hit(ndp_data, DATA_SIZE, MAX_KEY + 1);
-  //   auto end_sw = GET_TICKS;
-  //
-  //   auto start_hw = GET_TICKS;
-  //   pi_addr_data = NDP_DATA;
-  // #if defined(FS)
-  //   pi_data_size = 0x40;
-  // #else
-  //   pi_data_size = 0x1;
-  // #endif
-  //   pi_data_skey = MAX_KEY + 1;
-  //   pi_cmmd_code = 0;
-  //   pi_strt_rgst = START_CODE;
-  // #if defined(FS) && defined(DRIVER)
-  //   assert(write(fd_ndp_dev_a, (void *)ndp_ctrl, WRI_SIZE) == WRI_SIZE);
-  //   do {
-  //     assert(read(fd_ndp_dev_a, (void *)(ndp_ctrl + WRI_SIZE /
-  //     sizeof(uint64_t)),
-  //                 REA_SIZE) == REA_SIZE);
-  //   } while (!pi_stat_rgst);
-  // #else
-  //   while (!pi_stat_rgst)
-  //     ;
-  // #endif
-  //   uint64_t res_hw = pi_last_rslt;
-  //   auto end_hw = GET_TICKS;
-  //
-  //   printf("[%s] Hit_TSC_Best:  sw: %6lu ns, hw: %6lu ns (norm: %2.3f)\n",
-  //          res_sw == res_hw ? "PASS" : "FAIL", GET_ELAPS(start_sw, end_sw),
-  //          GET_ELAPS(start_hw, end_hw),
-  //          1.0 * GET_ELAPS(start_hw, end_hw) / GET_ELAPS(start_sw, end_sw));
-
-  /* ============================== Strided_TSC ==============================
-   */
-  //   uint64_t stride_bytes = 128;
-  //   uint64_t strided_elements = DATA_SIZE / (stride_bytes /
-  //   sizeof(uint64_t));
-  //
-  //   start_sw = GET_TICKS;
-  //   res_sw = strided_access(ndp_data, strided_elements, stride_bytes);
-  //   end_sw = GET_TICKS;
-  //
-  //   start_hw = GET_TICKS;
-  //   pi_addr_data = NDP_DATA;
-  //   pi_data_size = strided_elements;
-  //   pi_data_skey = stride_bytes;
-  //   pi_cmmd_code = 3;
-  //   pi_strt_rgst = START_CODE;
-  // #if defined(FS) && defined(DRIVER)
-  //   assert(write(fd_ndp_dev_a, (void *)ndp_ctrl, WRI_SIZE) == WRI_SIZE);
-  //   do {
-  //     assert(read(fd_ndp_dev_a, (void *)(ndp_ctrl + WRI_SIZE /
-  //     sizeof(uint64_t)),
-  //                 REA_SIZE) == REA_SIZE);
-  //   } while (!pi_stat_rgst);
-  // #else
-  //   while (!pi_stat_rgst)
-  //     ;
-  // #endif
-  //   res_hw = pi_last_rslt;
-  //   end_hw = GET_TICKS;
-  //
-  //   printf("[%s] Strided_TSC:     sw: %6lu ns, hw: %6lu ns (norm: %2.3f)\n",
-  //          res_sw == res_hw ? "PASS" : "FAIL", GET_ELAPS(start_sw, end_sw),
-  //          GET_ELAPS(start_hw, end_hw),
-  //          1.0 * GET_ELAPS(start_hw, end_hw) / GET_ELAPS(start_sw, end_sw));
-
-  /* =========================== PointerChase_TSC ============================
-   */
   auto start_sw = GET_TICKS;
-  auto res_sw = pointer_chase(ndp_nodes, num_nodes, NDP_DATA);
+  uint64_t res_sw = compare_n_hit(ndp_data, DATA_SIZE, MAX_KEY + 1);
   auto end_sw = GET_TICKS;
 
-  // ---------------------------------------------------------
-  // RUN 1: The Coherency Penalty (M -> S Downgrade)
-  // ---------------------------------------------------------
   auto start_hw = GET_TICKS;
   pi_addr_data = NDP_DATA;
-  pi_data_size = num_nodes;
-  pi_cmmd_code = 4;
+  pi_data_size = 0x1;
+  pi_data_skey = MAX_KEY + 1;
+  pi_cmmd_code = 0;
   pi_strt_rgst = START_CODE;
-#if defined(FS) && defined(DRIVER)
-  assert(write(fd_ndp_dev_a, (void *)ndp_ctrl, WRI_SIZE) == WRI_SIZE);
-  do {
-    assert(read(fd_ndp_dev_a, (void *)(ndp_ctrl + WRI_SIZE / sizeof(uint64_t)),
-                REA_SIZE) == REA_SIZE);
-  } while (!pi_stat_rgst);
-#else
+
   while (!pi_stat_rgst)
     ;
-#endif
-  auto res_hw = pi_last_rslt;
+
+  uint64_t res_hw = pi_last_rslt;
   auto end_hw = GET_TICKS;
 
-  printf("[%s] PntrChase (Dirty): sw: %6lu ns, hw: %6lu ns (norm: %2.3f)\n",
+  printf("[%s] Hit_TSC_Best:  sw: %6lu ns, hw: %6lu ns (norm: %2.3f)\n",
          res_sw == res_hw ? "PASS" : "FAIL", GET_ELAPS(start_sw, end_sw),
          GET_ELAPS(start_hw, end_hw),
          1.0 * GET_ELAPS(start_hw, end_hw) / GET_ELAPS(start_sw, end_sw));
 
+  /* ============================== Strided_TSC ==============================
+   */
+  // uint64_t stride_bytes = 128;
+  // uint64_t strided_elements = DATA_SIZE / (stride_bytes / sizeof(uint64_t));
+  //
+  // auto start_sw = GET_TICKS;
+  // auto res_sw = strided_access(ndp_data, strided_elements, stride_bytes);
+  // auto end_sw = GET_TICKS;
+  //
+  // auto start_hw = GET_TICKS;
+  // pi_addr_data = NDP_DATA;
+  // pi_data_size = strided_elements;
+  // pi_data_skey = stride_bytes;
+  // pi_cmmd_code = 3;
+  // pi_strt_rgst = START_CODE;
+  // while (!pi_stat_rgst)
+  //   ;
+  // auto res_hw = pi_last_rslt;
+  // auto end_hw = GET_TICKS;
+  //
+  // printf("[%s] Strided_TSC:     sw: %6lu ns, hw: %6lu ns (norm: %2.3f)\n",
+  //        res_sw == res_hw ? "PASS" : "FAIL", GET_ELAPS(start_sw, end_sw),
+  //        GET_ELAPS(start_hw, end_hw),
+  //        1.0 * GET_ELAPS(start_hw, end_hw) / GET_ELAPS(start_sw, end_sw));
+
+  /* =========================== PointerChase_TSC ============================
+   */
+  // auto start_sw = GET_TICKS;
+  // auto res_sw = pointer_chase(ndp_nodes, num_nodes, NDP_DATA);
+  // auto end_sw = GET_TICKS;
+  //
+  // // ---------------------------------------------------------
+  // // RUN 1: The Coherency Penalty (M -> S Downgrade)
+  // // ---------------------------------------------------------
+  // auto start_hw = GET_TICKS;
+  // pi_addr_data = NDP_DATA;
+  // pi_data_size = num_nodes;
+  // pi_cmmd_code = 4;
+  // pi_strt_rgst = START_CODE;
+  // while (!pi_stat_rgst)
+  //   ;
+  // auto res_hw = pi_last_rslt;
+  // auto end_hw = GET_TICKS;
+  //
+  // printf("[%s] PntrChase (Dirty): sw: %6lu ns, hw: %6lu ns (norm: %2.3f)\n",
+  //        res_sw == res_hw ? "PASS" : "FAIL", GET_ELAPS(start_sw, end_sw),
+  //        GET_ELAPS(start_hw, end_hw),
+  //        1.0 * GET_ELAPS(start_hw, end_hw) / GET_ELAPS(start_sw, end_sw));
+
   // ---------------------------------------------------------
   // RUN 2: The Isolated Baseline (Clean Read, No Snoops)
   // ---------------------------------------------------------
-  auto start_hw_clean = GET_TICKS;
-  pi_addr_data = NDP_DATA;
-  pi_data_size = num_nodes;
-  pi_cmmd_code = 4;
-  pi_strt_rgst = START_CODE;
-#if defined(FS) && defined(DRIVER)
-  assert(write(fd_ndp_dev_a, (void *)ndp_ctrl, WRI_SIZE) == WRI_SIZE);
-  do {
-    assert(read(fd_ndp_dev_a, (void *)(ndp_ctrl + WRI_SIZE / sizeof(uint64_t)),
-                REA_SIZE) == REA_SIZE);
-  } while (!pi_stat_rgst);
-#else
-  while (!pi_stat_rgst)
-    ;
-#endif
-  auto end_hw_clean = GET_TICKS;
-
-  printf("[PASS] PntrChase (Clean):               hw: %6lu ns\n",
-         GET_ELAPS(start_hw_clean, end_hw_clean));
+  // auto start_hw_clean = GET_TICKS;
+  // pi_addr_data = NDP_DATA;
+  // pi_data_size = num_nodes;
+  // pi_cmmd_code = 4;
+  // pi_strt_rgst = START_CODE;
+  // while (!pi_stat_rgst)
+  //   ;
+  //
+  // auto end_hw_clean = GET_TICKS;
+  //
+  // printf("[PASS] PntrChase (Clean):               hw: %6lu ns\n",
+  //        GET_ELAPS(start_hw_clean, end_hw_clean));
 
   return 0;
 }
